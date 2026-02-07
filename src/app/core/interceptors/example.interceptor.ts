f1bb65359783e09f82aecea2a70acdb18d05586e
netnoise <netnoise.dev@netnoise.eu>
2026-02-07 20:45:44 +0100

    Revert "Merge pull request #1"
    This reverts commit ced46631199e905c2d7f9eea39dbd6f26733cb43.

--- src/app/core/interceptors/example.interceptor.ts
+++
@@ -1,8 +0,0 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
-export const exampleInterceptor: HttpInterceptorFn = (req, next) => {
-  const modifiedReq = req.clone({
-    headers: req.headers.set('X-Example-Header', 'ExampleValue'),
-  });
-  return next(modifiedReq);
-};
